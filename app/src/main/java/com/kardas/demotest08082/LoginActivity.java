914df2de3157ffd14c8051f58ba6249011d06f68
VENKATESH KARDAS <kardasvenkatesh@gmail.com>
2022-08-08 19:23:23 +0530

    Add files via upload

---
+++ app/src/main/java/com/kardas/demotest08082/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.kardas.demo12;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kardas.demo12.databinding.ActivityMainBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
